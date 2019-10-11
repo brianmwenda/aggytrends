d01e9184861b4196817d916f72db646fdbc36f04
brianmwenda <54397128+brianmwenda@users.noreply.github.com>
2019-10-11 15:08:17 +0300

    Add files via upload

--- javascript.js
+++ javascript.js
@@ -1,10 +1,11 @@
 
- 
+
 document.querySelector('.button3').addEventListener('click',function(){
     alert('Thank you for getting in touch here is my contact '+ '0702767516');
 });
 document.querySelector('.button4').addEventListener('click',function(){
-    alert('Thank you for getting in touch here is my contact '+ <h1>0704903968</h1>);
+    alert('Thank you for getting in touch here is my contact '+ '0704903968');
+    
 });
 
 
@@ -18,19 +19,19 @@ function getInfo(){
     };
     var username = document.getElementById('username').value;
     var password = document.getElementById('password').value;
-    
+
    // document.querySelector('.imageuploader').style.display='none';
 
     if(username === objectPeople.username && password === objectPeople.password){
-       
+
             document.querySelector('.imageuploader').style.display = 'inline';
             document.querySelector('.imageuploader').style.border = '100px';
         image =  document.querySelector('.img1').value;
-            
-        
+
+
         document.getElementById('image-uploader').value;
        edit();
-        
+
     };
 }
 
@@ -51,7 +52,7 @@ function editItem() {
     this.className = "edit";
     var input= this.querySelectorAll("input");
     input.setSelectionRange(0, input.value.length);
-    
+
 }
 function updateItem(){
     this.previousElementSibling.innerHTML = this.value;
@@ -62,10 +63,6 @@ function itemKeypress(event) {
     if(event.which=== 13){
         updateItem.call(this);
     }
-    
+
 }
 }*/
-
-
-
-console.log(dir_photos = '');
